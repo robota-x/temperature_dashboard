32226bb2b3208131b11123b3842bcb656b09efbf
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-05-22 21:02:12 +0100

    refactor code and uuid to have consistent values per event, add event to error logging

--- python/temperature_sensor_endpoint.py
+++ python/temperature_sensor_endpoint.py
@@ -1,6 +1,5 @@
 import boto3
-import time
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
@@ -13,7 +12,7 @@ def log_to_s3(key, body):
     )
 
 
-def log_entry(event):
+def log_entry(event, timestamp, uuid):
     sensor_id, temperature, humidity = event.split(';')
 
     boto3.client('cloudwatch').put_metric_data(
@@ -21,7 +20,7 @@ def log_entry(event):
         MetricData=[
             {
                 'MetricName': 'temperature',
-                'Timestamp': datetime.now(),
+                'Timestamp': timestamp,
                 'Value': float(temperature),
                 'Unit': 'Count',
                 'Dimensions': [
@@ -32,8 +31,8 @@ def log_entry(event):
                 ],
             },
             {
-                'MetricName': 'temperature',
-                'Timestamp': datetime.now(),
+                'MetricName': 'humidity',
+                'Timestamp': timestamp,
                 'Value': float(humidity),
                 'Unit': 'Percent',
                 'Dimensions': [
@@ -46,18 +45,18 @@ def log_entry(event):
         ]
     )
     log_to_s3(
-        key=f'raw/{uuid.uuid4()}',
-        body=f'{int(time.time())};{event}'
+        key=f'raw/{uuid}',
+        body=f'{int(timestamp.timestamp())} : {event}'
     )
 
 
-def log_error(e, event):
+def log_error(error, event, timestamp, uuid):
     boto3.client('cloudwatch').put_metric_data(
         Namespace='temperature_sensor',
         MetricData=[
             {
                 'MetricName': 'error_count',
-                'Timestamp': datetime.now(),
+                'Timestamp': timestamp,
                 'Value': 1,
                 'Unit': 'Count',
             }
@@ -65,16 +64,28 @@ def log_error(e, event):
     )
 
     log_to_s3(
-        key=f'error/{uuid.uuid4()}',
-        body=str(e),
+        key=f'error/{uuid}',
+        body=f'{timestamp} : {event} : {error}',
     )
 
 
 def lambda_handler(event, _):
+    timestamp = datetime.now()
+    uuid = uuid4()
+
     try:
-        log_entry(event)
+        log_entry(
+            event=event,
+            timestamp=timestamp,
+            uuid=uuid
+        )
         return 'All good'
 
     except Exception as e:
-        log_error(e, event)
+        log_error(
+            error=e,
+            event=event,
+            timestamp=timestamp,
+            uuid=uuid
+        )
         return 'Something went wrong'
