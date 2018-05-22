687e71dc31eb7752f0077df94117fb0a983abd04
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-05-22 22:57:42 +0100

    update wifi_client to allow a success callback

--- esp8266/main.lua
+++ esp8266/main.lua
@@ -1,10 +1,17 @@
+dofile('config.lua')
+
 require('ambient_read')
 require('wifi_client')
+
 
 local temp, hum = ambient_read(DHT22_PIN)
 print('temp: ' .. temp .. ' humidity: ' .. hum)
 
-connect_wifi(WIFI_SSID, WIFI_PASSWORD)
+function success_hand(event)
+    print('outer success' .. event.IP .. ' : ' .. event.gateway)
+end
+
+connect_wifi(WIFI_SSID, WIFI_PASSWORD, success_hand)
 
 -- print('now sleeping 10 seconds')
 -- node.dsleep(10 * 1000000, 4)
