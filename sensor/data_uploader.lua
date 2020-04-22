ce43e44a88d3ce0fe15ca67bf5c7a3b7d92b2143
Lorenzo Turrino <neuronefunzionante@gmail.com>
2020-04-22 19:11:06 +0100

    upload actual data

--- sensor/data_uploader.lua
+++ sensor/data_uploader.lua
@@ -2,10 +2,11 @@ function upload_ambient_data(endpoint, api_token, temperature, humidity, callbac
     print('uploading: ' .. temperature .. 'C ' .. humidity .. '% to ' .. endpoint)
 
     local auth_header = 'Authorisation: Bearer ' .. api_token .. '\r\n'
+    local payload = temperature .. ' ' .. humidity
     http.post(
         endpoint,
         auth_header,
-        'Hello friends',
+        payload,
         callback
     )
 end
